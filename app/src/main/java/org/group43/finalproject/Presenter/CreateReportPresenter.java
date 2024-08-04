80846c59dd4ba4ad19aee7caac886ee74d8b68e6
yoloanda <yolandaxiaoqi@gmail.com>
2024-08-04 14:01:12 -0400

    feat: images now included in report

--- app/src/main/java/org/group43/finalproject/Presenter/CreateReportPresenter.java
+++ app/src/main/java/org/group43/finalproject/Presenter/CreateReportPresenter.java
@@ -4,25 +4,35 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Environment;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 
+import org.group43.finalproject.Model.ImageDownloadTask;
+import org.group43.finalproject.Model.ImageRunnable;
 import org.group43.finalproject.R;
 import org.group43.finalproject.Model.Artifact;
 import org.group43.finalproject.View.CreateReportFragment;
@@ -30,19 +40,32 @@ import org.group43.finalproject.View.CreateReportFragment;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class CreateReportPresenter {
     private static final int PAGE_WIDTH = 612;
     private static final int PAGE_HEIGHT = 400;
-    private static final int IMAGE_WIDTH = 200;
-    private static final int IMAGE_HEIGHT = 200;
+    private static final int IMAGE_WIDTH = 230;
+    private static final int IMAGE_HEIGHT = 160;
 
     private final CreateReportFragment view;
+    private final Queue<Artifact> artifactQueue;
+    private int pageNum;
 
     public CreateReportPresenter(CreateReportFragment view) {
         this.view = view;
+        artifactQueue = new LinkedList<>();
+        pageNum = 1;
     }
 
     public void generateReport(Button checkedButton, EditText checkedButtonText, boolean picDescOnly) {
@@ -52,29 +75,60 @@ public class CreateReportPresenter {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    createPDF(snapshot, picDescOnly);
+                    for (DataSnapshot artifactSnapshot : snapshot.getChildren()) {
+                        artifactQueue.add(artifactSnapshot.getValue(Artifact.class));
+                    }
+
+                    PdfDocument pdf = new PdfDocument();
+                    addPageToPDF(pdf, picDescOnly);
                 } else {
                     view.showMessage("Error: No artifacts found matching filter. PDF Report not generated.");
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
         });
     }
 
-    private void createPDF(DataSnapshot snapshot, boolean picDescOnly) {
-        PdfDocument pdf = new PdfDocument();
-        int pageNum = 1;
+    private void addPageToPDF(PdfDocument pdf, boolean picDescOnly) {
+        Artifact artifact = artifactQueue.poll();
 
-        for (DataSnapshot artifactSnapshot : snapshot.getChildren()) {
-            addPageToReport(pdf, Objects.requireNonNull(artifactSnapshot
-                    .getValue(Artifact.class)), picDescOnly, pageNum);
-            pageNum++;
+        if (artifact == null) {
+            downloadPDF(pdf);
+            return;
         }
 
-        downloadPDF(pdf);
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(PAGE_WIDTH, PAGE_HEIGHT, pageNum).create();
+        PdfDocument.Page page = pdf.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+
+        if (!picDescOnly) {
+            drawArtifactInfoToPDF(canvas, artifact);
+        }
+
+        drawBasicReportInfo(canvas, artifact, pageNum);
+
+        if (artifact.getFileType().equals(view.getResources().getString(R.string.image))) {
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference imageRef = storageRef.child("img/" + artifact.getFile());
+
+            imageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+                    Bitmap resizeBitmap = Bitmap.createScaledBitmap(bitmap, IMAGE_WIDTH, IMAGE_HEIGHT, true);
+                    canvas.drawBitmap(resizeBitmap, 20, 20, null);
+                    pdf.finishPage(page);
+                    pageNum++;
+                    addPageToPDF(pdf, picDescOnly);
+                }
+            });
+        } else {
+            pdf.finishPage(page);
+            pageNum++;
+            addPageToPDF(pdf, picDescOnly);
+        }
     }
 
     private Query getArtifactsToInclude(String option, EditText checkedButtonText) {
@@ -94,64 +148,39 @@ public class CreateReportPresenter {
                 .equalTo(checkedButtonText.getText().toString().trim());
     }
 
-    private void addPageToReport(PdfDocument pdf, Artifact artifact, boolean PicDescOnly, int pageNum) {
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(PAGE_WIDTH, PAGE_HEIGHT, pageNum).create();
-        PdfDocument.Page page = pdf.startPage(pageInfo);
-        Canvas canvas = page.getCanvas();
-        TextPaint mTextPaint = new TextPaint();
+    private void drawBasicReportInfo(Canvas canvas, Artifact artifact, int pageNum) {
         Paint paint = new Paint();
 
-        if (artifact.getFileType().equals(view.getResources().getString(R.string.image))) {
-            //drawImageToPdf(canvas, artifact);
-        }
-
-        if (!PicDescOnly) {
-            drawArtifactInfoToPDF(canvas, artifact, paint);
-        }
-
-        StaticLayout descLayout = new StaticLayout(artifact.getDescription(), mTextPaint, 320, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        StaticLayout descLayout = new StaticLayout(artifact.getDescription(), new TextPaint(), 320, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
         canvas.save();
-        canvas.translate(250, 30);
+        canvas.translate(270, 30);
         descLayout.draw(canvas);
         canvas.restore();
 
         canvas.drawText(view.getResources().getString(R.string.app_name), 10, 390, paint);
         canvas.drawText(String.valueOf(pageNum), 580, 390, paint);
-
-        pdf.finishPage(page);
     }
 
-    private void drawImageToPdf(Canvas canvas, Artifact artifact) {
-        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-        StorageReference imageRef = storageRef.child("img/" + artifact.getFile());
-
-        imageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(bytes -> {
-            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            Bitmap resizeBitmap = Bitmap.createScaledBitmap(bitmap, IMAGE_WIDTH, IMAGE_HEIGHT, true);
-
-            canvas.drawBitmap(resizeBitmap, 0, 0, null);
-        }).addOnFailureListener(exception -> {});
-    }
-
-    private void drawArtifactInfoToPDF(Canvas canvas, Artifact artifact, Paint paint) {
+    private void drawArtifactInfoToPDF(Canvas canvas, Artifact artifact) {
         TextPaint nameTextPaint = new TextPaint();
+        Paint paint = new Paint();
         StaticLayout layout;
 
         nameTextPaint.setTextSize(16);
 
-        layout = new StaticLayout(artifact.getName(), nameTextPaint, 210, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        layout = new StaticLayout(artifact.getName(), nameTextPaint, 230, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
         canvas.save();
-        canvas.translate(10, 210);
+        canvas.translate(20, 190);
         layout.draw(canvas);
         canvas.restore();
 
-        canvas.drawText(view.getResources().getString(R.string.lotNumShort) + artifact.getLotNumber(), 10, 280, paint);
-        canvas.drawText(view.getResources().getString(R.string.period) + ": " + artifact.getPeriod(), 10, 300, paint);
+        canvas.drawText(view.getResources().getString(R.string.lotNumShort) + artifact.getLotNumber(), 20, 280, paint);
+        canvas.drawText(view.getResources().getString(R.string.period) + ": " + artifact.getPeriod(), 20, 300, paint);
 
-        canvas.drawText(view.getResources().getString(R.string.category) + ": ", 10, 321, paint);
+        canvas.drawText(view.getResources().getString(R.string.category) + ": ", 20, 321, paint);
         layout = new StaticLayout(artifact.getCategory(), new TextPaint(), 150, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
         canvas.save();
-        canvas.translate(65, 310);
+        canvas.translate(75, 310);
         layout.draw(canvas);
         canvas.restore();
     }
