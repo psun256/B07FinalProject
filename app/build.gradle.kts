59e6037293520e9015bea4be6971d71860d3ce11
psun256 <paulsun700@gmail.com>
2024-08-06 20:33:26 -0400

    fix: fixed gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.espresso.core)
     testImplementation("org.mockito:mockito-core:5.+")
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
